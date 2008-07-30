801e99d1eb34f75de7b4100d25bfcc8f57b540a7
Robert William Fuller <hydrologiccycle@gmail.com>
2008-07-30 18:28:27 +0000

    add testing for list

--- test/test.c
+++ test/test.c
@@ -45,8 +45,11 @@ cc_category(Foo, Blastme,
     )
 
 
+#include "firstcls.h"
+
 int main(int argc, char *argv[])
 {
+#if 0
     printf("sizeof(cc_arg_t) == %lu\n", sizeof(cc_arg_t));
     printf("sizeof(cc_class_object) == %lu\n", sizeof(cc_class_object));
     printf("sizeof(Foo) == %lu\n", sizeof(Foo));
@@ -85,6 +88,56 @@ int main(int argc, char *argv[])
     //n = a;
 
     //n = a.i;
+#else
 
+    cc_obj list = as_obj(cc_msg(&FcList, "new"));
+
+    if (as_int(cc_msg(list, "isEmpty"))) {
+        printf("list starts out as empty\n");
+    }
+
+    //cc_msg(as_obj(cc_msg(list, "prefix", by_str("doh"))), "affix", by_str("bleh"));
+    //cc_msg(list, "prefix", by_str("doh"), by_str("bleh"));
+    cc_msg(list, "affix", by_str("doh"), by_str("bleh"));
+
+    if (!as_int(cc_msg(list, "isEmpty"))) {
+        printf("list is no longer empty\n");
+    }
+
+    cc_obj cursor = as_obj(cc_msg(list, "cursor"));
+
+#if 0
+    char *item;
+    for (item = as_str(cc_msg(cursor, "first"));  item;  item = as_str(cc_msg(cursor, "next"))) {
+        printf("item in list is \"%s\";  current is %s\n", item, as_str(cc_msg(cursor, "current")));
+    }
+#else
+    char *item;
+    for (item = as_str(cc_msg(cursor, "last"));  item;  item = as_str(cc_msg(cursor, "previous"))) {
+        printf("item in list is \"%s\";  current is %s\n", item, as_str(cc_msg(cursor, "current")));
+    }
+#endif
+
+    printf("after removePrefix:\n");
+    item = as_str(cc_msg(list, "removePrefix"));
+    for (item = as_str(cc_msg(cursor, "first"));  item;  item = as_str(cc_msg(cursor, "next"))) {
+        printf("item in list is \"%s\"\n", item);
+    }
+    
+    printf("after removeAffix:\n");
+    item = as_str(cc_msg(list, "removeAffix"));
+    for (item = as_str(cc_msg(cursor, "first"));  item;  item = as_str(cc_msg(cursor, "next"))) {
+        printf("item in list is \"%s\"\n", item);
+    }
+
+#if 0
+    printf("after removePrefix:\n");
+    item = as_str(cc_msg(list, "removePrefix"));
+    for (item = as_str(cc_msg(cursor, "first"));  item;  item = as_str(cc_msg(cursor, "next"))) {
+        printf("item in list is \"%s\"\n", item);
+    }
+#endif
+    
+#endif
     return (EXIT_SUCCESS);
 }
