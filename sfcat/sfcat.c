1a124c5e62c9fd5a6a6b1598cd27a527bf6dd147
Robert William Fuller <hydrologiccycle@gmail.com>
2009-01-25 03:31:54 +0000

    update sfcat.c

--- sfcat/sfcat.c
+++ sfcat/sfcat.c
@@ -14,6 +14,9 @@
 **
 ** You should have received a copy of the GNU General Public License
 ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+** TODO:  always check return code of sf_command, sf_close, and sf_write*
+**
 */
 
 #include "opt.h"
