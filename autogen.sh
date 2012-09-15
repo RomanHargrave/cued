d8661591fbf0404638c3589142444187afe7595e
Robert William Fuller <hydrologiccycle@gmail.com>
2012-09-15 10:07:33 -0400

    ongoing

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-aclocal
-autoconf
-autoheader
-automake -a --foreign
+#aclocal
+#autoconf
+#autoheader
+#automake -a --foreign -Wall
+#autoreconf --install --symlink --force -Wall -vv
+autoreconf --install --symlink --force -Wall -v
